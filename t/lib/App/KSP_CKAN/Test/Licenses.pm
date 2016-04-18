c336d62395b519caaadd091e695ac45ba53e4433
Leon Wright <techman83@gmail.com>
2016-04-18 17:55:32 +0800

    Testing shim package for Licenses role

---
+++ t/lib/App/KSP_CKAN/Test/Licenses.pm
@@ -0,0 +1,9 @@
+package App::KSP_CKAN::Test::Licenses;
+use Method::Signatures 20140224;
+use App::KSP_CKAN::Tools::Http;
+use Moo;
+use namespace::clean;
+
+with('App::KSP_CKAN::Roles::Licenses');
+
+1;
